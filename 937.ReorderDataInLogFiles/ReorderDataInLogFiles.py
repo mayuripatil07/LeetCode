ef632c1b2525da42d6057a44e6e74aa6f3a83d1a
Mayuri098 <mpatil7@binghamton.edu>
2020-05-11 18:01:46 -0400

    11 May 2020

---
+++ 937.ReorderDataInLogFiles/ReorderDataInLogFiles.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        digit_log = []
+        letter_log= []
+        for log in logs:
+            if log.split()[1].isdigit():
+                digit_log.append(log)
+            else:
+                letter_log.append(log)
+
+        letter_log.sort(key = lambda x : (x.split( )[1:], x.split( )[0]))
+
+        return letter_log + digit_log
+      
