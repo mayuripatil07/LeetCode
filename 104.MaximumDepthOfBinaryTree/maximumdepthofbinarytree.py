2a36b1fa1715fb7cd441cc60656149f34a1da91c
Mayuri098 <mpatil7@binghamton.edu>
2020-06-07 19:22:35 -0400

    07 June 2020

---
+++ 104.MaximumDepthOfBinaryTree/maximumdepthofbinarytree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        queue = deque()
+        queue.append(root)
+        count = 0
+        while(queue):
+            count += 1
+            n = len(queue)
+            for i in range(n):
+                curr_val = queue.popleft()
+                if curr_val.left:
+                    queue.append(curr_val.left)
+
+                if curr_val.right:
+                    queue.append(curr_val.right)
+
+        return count
+        
