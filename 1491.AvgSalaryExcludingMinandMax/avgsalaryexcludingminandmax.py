7c3ac22bdf20c0bef66fa7c63b95cc7d266455d2
Mayuri098 <mpatil7@binghamton.edu>
2020-06-30 19:39:02 -0400

    30 June 2020

---
+++ 1491.AvgSalaryExcludingMinandMax/avgsalaryexcludingminandmax.py
@@ -0,0 +1,9 @@
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        salary.sort()
+        s = 0
+        count = 0
+        for i in range(1,len(salary)-1):
+            s += salary[i]
+            count += 1
+        return s/count
